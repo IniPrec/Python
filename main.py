dd651840c7041f5e63177377f4fcb03dedb71c41
IniPrec <preciousinioluwa9@gmail.com>
2024-07-17 12:59:17 +0100

    Tackling some errors *sighs*

--- main.py
+++ main.py
@@ -19,34 +19,32 @@ quit = False
 print("Welcome to the ATM!")
 attempts = 0
 
-pin = int(input("Enter your four-digit pin: "))
-if pin == user['pin']:
-	print("What do you want to do?\n")
-	choice = (input(print("1. Withdrawal\n2. Balance\n3. Quit\n")))
-	match choice:
-		case "1": 
-			withdrawal()
-		case "2":
-			balance()
-		case "3":
-			quit = True
-		case _:
-			print("Invalid Choice! Choose 1 for withdrawal, 2 for balance enquiry and 3 to quit.")
-			
-	if choice != "3":
-		print("Would you like to continue?")
-		proceed = input("1. Yes\n2. No\n")
-		match proceed:
-			case "1":
-				
+while not quit:
+	pin = int(input("Enter your four-digit pin: "))
+	if pin == user['pin']:
+		print("What do you want to do?\n")
+		input("1. Withdrawal\n2. Balance\n3. Quit\n")
+		choice = input()
+		match choice:
+			case "1": 
+				withdrawal()
 			case "2":
+				balance()
+			case "3":
 				quit = True
-				print("Thank you for using our services. Have a good day!")
-	
-else:
-	while attempts == 3:
-		attempts += 1
-		if attempts != 3:
-			print("Incorrect Pin! Try again!")
-		else:
-			print("Incorrect Pin! Your card has been blocked due to multiple failed attempts")
+			case _:
+				print("Invalid Choice! Choose 1 for withdrawal, 2 for balance enquiry and 3 to quit.")
+	else:
+		while attempts == 3:
+			attempts += 1
+			if attempts != 3:
+				print("Incorrect Pin! Try again!")
+			else:
+				print("Incorrect Pin! Your card has been blocked due to multiple failed attempts")
+
+	proceed = input("Would you like to perform another transaction? (1. Yes / 2. No): ")
+	if proceed == "2":
+		quit = True
+
+print("Thank you for using our services. Have a good day!")
+
